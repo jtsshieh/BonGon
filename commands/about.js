1de402dd6bb536e836893bd56319cc07abc7c501
jtsshieh <jtsshieh@outlook.com>
2017-08-29 16:38:05 -0400

    about command

---
+++ commands/about.js
@@ -0,0 +1,38 @@
+const Eris = require("eris");
+exports.run = (bot, msg, args) => {
+    var embeds = {
+        title: "About BonGon",
+        description: "A few important things",
+        author: {
+            name: msg.author.username,
+            icon_url: msg.author.avatarURL
+        },
+        color: 0x00afff,
+        fields: [
+            {
+                name: "Licence",
+                value: "BonGon is licenced under the MIT licence",
+                inline : true
+            },
+            {
+                name: "Privacy Policy",
+                value: "You agree that this bot will use you and your guild members information to enhance your expierience. \nIf you do not agree to this, either kick the bot off you guild or the guild with the bot.",
+                inline: true
+            },
+            {
+                name: "Credits",
+                value: "Creator: jtsshieh#6242 \n Library: Eris \n A HUGE Helper: EiJay #7711 \n on the Unofficial Discord API server"
+            }
+        ],
+        footer: {
+            text: `This message was delivered to ${msg.author.username}`
+        }
+    }
+    msg.channel.createMessage( { embed : embeds } )
+}
+module.exports.help = {
+    description: "Displays the about page",
+    usage: "j!about",
+    permlevel: 0,
+    category: "Other"
+}
