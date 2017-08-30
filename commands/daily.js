e59adc36c53a64845e0c0afa1b773d8f270dd2a3
jtsshieh <jtsshieh@outlook.com>
2017-08-29 20:18:41 -0400

    continuation of last

--- commands/daily.js
+++ commands/daily.js
@@ -8,11 +8,11 @@ exports.run = (bot, msg, args) => {
                 if(arrayOfObjects[msg.author.id]['daily'] == false){
                     arrayOfObjects[msg.author.id]['gons'] +=100
                     arrayOfObjects[msg.author.id]['daily'] = true
-                    var embedToUse = bank.bank(msg, bot, 2, ["Gons Revieved","New Balance"], [`***You have recived 100 gons from the bank for your daily payout***`, `***Your new balance is ${arrayOfObjects[msg.author.username]['gons']} gons***`], [true,true])
+                    var embedToUse = bank.bank(msg, bot, 2, ["Gons Revieved","New Balance"], [`***You have recived 100 gons from the bank for your daily payout***`, `***Your new balance is ${arrayOfObjects[msg.author.id]['gons']} gons***`], [true,true])
                     msg.channel.createMessage({embed:embedToUse})
                 }
                 else{
-                    var embedToUse = bank.bank(msg, bot, 2, ["Reject","Balance"], [`***You have already recived your daily gons, check back tomorrow (UTC-4 DST EST or UTC-5 EST) to get your next payout***`, `***Your current balance is ${arrayOfObjects[msg.author.username]['gons']} gons***`], [true,true])
+                    var embedToUse = bank.bank(msg, bot, 2, ["Reject","Balance"], [`***You have already recived your daily gons, check back tomorrow (UTC-4 DST EST or UTC-5 EST) to get your next payout***`, `***Your current balance is ${arrayOfObjects[msg.author.id]['gons']} gons***`], [true,true])
                     msg.channel.createMessage({embed:embedToUse})
                     return;
                 }
@@ -21,7 +21,7 @@ exports.run = (bot, msg, args) => {
                 arrayOfObjects[msg.author.id] = {};
                 arrayOfObjects[msg.author.id]['gons'] = 100;
                 arrayOfObjects[msg.author.id]['daily'] = true
-                var embedToUse = bank.bank(msg, bot, 3, ["Note","Gons Recieved","New Balance"], ["***Since you did not have an account, an account has been created***" ,`***You have recived 100 gons from the bank for your daily payout.***`, `***Your new balance is ${arrayOfObjects[msg.author.username]['gons']} gons***`], [true, true,true])
+                var embedToUse = bank.bank(msg, bot, 3, ["Note","Gons Recieved","New Balance"], ["***Since you did not have an account, an account has been created***" ,`***You have recived 100 gons from the bank for your daily payout.***`, `***Your new balance is ${arrayOfObjects[msg.author.id]['gons']} gons***`], [true, true,true])
                 msg.channel.createMessage({embed:embedToUse})
             }
             fs.writeFile('./users.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
--- commands/gons.js
+++ commands/gons.js
@@ -9,14 +9,14 @@ exports.run = (bot, msg, args) => {
         }
         var arrayOfObjects = JSON.parse(content)
             if(arrayOfObjects[msg.author.id]) {
-                var embedToUse = bank.bank(msg, bot, 1, ["Available Gons"], [`***You have ${arrayOfObjects[msg.author.username]['gons']} gons***`], [true])
+                var embedToUse = bank.bank(msg, bot, 1, ["Available Gons"], [`***You have ${arrayOfObjects[msg.author.id]['gons']} gons***`], [true])
                 msg.channel.createMessage({embed:embedToUse})
             }
             else{
                 arrayOfObjects[msg.author.id] = {};
                 arrayOfObjects[msg.author.id]['gons'] = 0;
                 arrayOfObjects[msg.author.id]['daily'] = false;
-                var embedToUse = bank.bank(msg, bot, 2, ["Note","Available Gons"], ["***Since you did not have an account, an account has been created***",`***You have ${arrayOfObjects[msg.author.username]['gons']} gons***`], [true,true])
+                var embedToUse = bank.bank(msg, bot, 2, ["Note","Available Gons"], ["***Since you did not have an account, an account has been created***",`***You have ${arrayOfObjects[msg.author.id]['gons']} gons***`], [true,true])
                 msg.channel.createMessage({embed:embedToUse})
                 fs.writeFile('./users.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
                     if (err) throw err
