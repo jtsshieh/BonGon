618ddffcd38243bcc4e472987f114011971b49bf
jtsshieh <jtsshieh@outlook.com>
2017-09-17 22:13:45 -0400

    updates to the server

--- Web/server.js
+++ Web/server.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var app = express();
 
-app.set("view engine", "ejs");
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static("Web/public"));
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
     res.render("../web/views/pages/index");
 });
 
-
-app.listen(5000);
-console.log("Dashboard is now running");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
