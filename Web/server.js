53a98f9f9f83c56a2ccfc52f4d112a4dcbb34bfd
jtsshieh <jtsshieh@outlook.com>
2017-09-17 22:30:40 -0400

    Tiny changes for the linter

--- Web/server.js
+++ Web/server.js
@@ -1,18 +1,18 @@
 var express = require("express");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
     res.render("pages/index");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+    console.log("Dashboard is running on port", app.get("port"));
 });
