014b8642a684954fb4e1f490375c5b316752c0a7
jtsshieh <jtsshieh@outlook.com>
2017-10-03 18:34:41 -0400

    automated help command

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,30 @@ exports.run = (bot, msg, args) => {
     let embed = new bot.RichEmbed();
     let prefix = bot.settings.get(msg.channel.guild.id).prefix;
     if (!args[0]) {
-        //
+        let categories = [];
+        let commands = Array.from(bot.commands.keys());
+        commands.forEach(function (x){
+            if(!categories.includes(bot.commands.get(x).help.category)){
+                categories.push(bot.commands.get(x).help.category);
+            }
+        });
+        let embed = new bot.RichEmbed();
+        categories.forEach(function(x){
+            let cat = '';
+            commands.forEach(function (command){
+                if(bot.commands.get(command).help.category == x){
+                    cat = cat + command + '\n';
+                }
+            });
+            embed.addField(x,cat);
+        });
+        embed.setTitle('BonGon Help');
+        embed.setDescription(`To view help for a specific command, type ${prefix}help`);
+        embed.setAuthor('BonGon', bot.user.avatarURL);
+        embed.setColor(0x00afff);
+        embed.setFooter(`The prefix for the server is ${prefix}`);
+        embed.setTimestamp();
+        msg.channel.createMessage({embed});
     } else{
         let command;
         if (bot.commands.has(args[0])) {
