1a9782813e0281b121bbd28aff58f20008f32c52
jtsshieh <jtsshieh@outlook.com>
2017-10-06 23:01:16 -0400

    buildpacks

---
+++ shutdown.js
@@ -0,0 +1,19 @@
+exports.run = (bot, msg) => {
+    if(msg.author.id == '236279900728721409'){
+        msg.channel.createMessage('Bot is now shutting down...');
+        process.exit(0);
+    }
+};
+
+exports.conf = {
+    aliases: ['shut'],
+    guildOnly: false
+};
+
+exports.help = {
+    name: 'shutdown',
+    description: 'Shutdowns the bot.',
+    usage: 'shutdown',
+    permlevel: 11,
+    category: 'Owner'
+};
