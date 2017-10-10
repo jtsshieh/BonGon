103c5087639ab47364adbb6e76baf308a82cd26d
jtsshieh <jtsshieh@outlook.com>
2017-10-09 22:10:58 -0400

    volume file

--- commands/volume.js
+++ commands/volume.js
@@ -2,14 +2,14 @@ exports.run = (bot, msg, args, prefix) => {
     let server = bot.MusicVariables(msg.member.guild.id);
     if (!args[0]) {
         let embed = bot.buildPlayer(`The current volume for this queue is ${server.dispatcher.volume}%. Use ${prefix}volume to change it`, [], [], [], server.nowPlaying.thumbnail, [server.nowPlaying.title, server.nowPlaying.thumbnail]);
-        return msg.channel.createMessage(embed);
+        return msg.channel.createMessage({embed});
     }
 
-    if (args[1] < 0 || args[1] > 100) {
+    if (args[0] < 0 || args[0] > 100) {
         return msg.channel.createMessage('Invalid Volume! Please provide a volume from 0 to 100.');
     }
 
-    let volume = Number(args[1]) / 100;
+    let volume = Number(args[0]) / 100;
     if (server.dispatcher) {
         server.dispatcher.setVolume(volume);
         msg.channel.createMessage(`Volume set: ${volume}%`);
