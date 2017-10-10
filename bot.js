48bb9d29639e706c7701a384a25606f34a1c3880
jtsshieh <jtsshieh@outlook.com>
2017-10-10 16:49:15 -0400

    welcome addrole for my guild

--- bot.js
+++ bot.js
@@ -91,12 +91,18 @@ bot.on('guildCreate', (guild) => {
 });
 
 bot.on('guildMemberAdd', (guild, member) => {
+    if(guild.id == '353242685961928704'){
+        let role = guild.roles.find(role => role.name == 'Member');
+        member.addRole(role.id);
+    }
     if (bot.settings.get(guild.id) == undefined){
         bot.settings.set(guild.id, {'prefix': 'b!', 'modlogs':'mod-logs', 'welcome': 0, 'welcomeMessage': 'Welcome to the server {mention}', 'welcomeChannel' : 'general', 'Perm2': 'Trusted', 'Perm3': 'Moderator', 'Perm4' : 'Admin', 'Perm5': 'Owner'});
     }
     if(!bot.settings.get(guild.id).welcome == 0 && !bot.settings.get(guild.id).welcomeMessage == ''){
         let welcome = bot.settings.get(guild.id).welcomeMessage.replace(/{user}}/gi, member.username);
         welcome = welcome.replace(/{mention}}/gi, member.mention);
+        welcome = welcome.replace(/{user}}/gi, member.username);
+        welcome = welcome.replace(/{discrim}}/gi, member.driscriminator);
         let welcomeChannel = guild.channels.find('name', bot.settings.get(guild.id).welcomeChannel);
         if(welcomeChannel){
             welcomeChannel.createMessage(welcome);
