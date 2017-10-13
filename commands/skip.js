0925e6cfaa14d30d0f6f459d8c4ab160ede5c6dd
jtsshieh <jtsshieh@outlook.com>
2017-10-12 21:55:32 -0400

    SKip command beta

---
+++ commands/skip.js
@@ -0,0 +1,26 @@
+exports.run = (bot, msg) => {
+    let server = bot.MusicVariables(msg.member.guild.id);
+    if (!server) return msg.channel.createMessage('There is nothing to skip');
+    if (!server.dispatcher) return msg.channel.createMessage('There is nothing to skip');
+    if(!server.nowPlaying) return msg.channel.createMessage('There is nothing to skip');
+    let members = msg.channel.guild.channels.find(channel => channel.id == server.dispatcher.channelID).voiceMembers.size;
+    server.nowPlaying.votes +=1;
+    msg.channel.createMessage('1 vote added');
+    if(Math.round((members-1)/2) < server.nowPlaying.votes){
+        server.dispatcher.stop();
+        msg.channel.createMessage('Enough votes have been aquired, skipped the song');
+    }
+};
+
+exports.conf = {
+    aliases: [],
+    guildOnly: true
+};
+
+exports.help = {
+    name: 'skip',
+    description: 'Adds a vote to skip the song.',
+    usage: 'skip',
+    permlevel: 0,
+    category: 'Music'
+};
