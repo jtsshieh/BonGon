ce76fc893fb628105ff3220d69e3ae073958f767
jtsshieh <jtsshieh@outlook.com>
2017-10-17 22:27:43 -0400

    More changes to the skip command

--- commands/skip.js
+++ commands/skip.js
@@ -4,10 +4,23 @@ exports.run = (bot, msg) => {
     if (!server.dispatcher) return msg.channel.createMessage('There is nothing to skip');
     if(!server.nowPlaying) return msg.channel.createMessage('There is nothing to skip');
     let members = msg.channel.guild.channels.find(channel => channel.id == server.dispatcher.channelID).voiceMembers.size;
-    server.nowPlaying.votes = 0;
-    server.nowPlaying.votes +=1;
-    msg.channel.createMessage('1 vote added');
-    if(Math.round((members-1)/2) <= server.nowPlaying.votes){
+    if(!server.nowPlaying.votes) server.nowPlaying.votes = [];
+    let found = false;
+    server.nowPlaying.votes.forEach(
+        function(x){
+            if(x == msg.author.id){
+                found = true;
+                return;
+            }
+        });
+    if(found == false){
+        server.nowPlaying.votes.push(msg.author.id );
+        msg.channel.createMessage('1 vote added');
+    }
+    else{
+        msg.channel.createMessage('You have already voted');
+    }
+    if(Math.round((members-1)/2) <= server.nowPlaying.votes.length){
         server.dispatcher.stopPlaying();
         msg.channel.createMessage('Enough votes have been aquired, skipped the song');
     }
