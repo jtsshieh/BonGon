be4272223bb196a6b7f1ddb0f3247da856cfb575
jtsshieh <jtsshieh@outlook.com>
2017-10-21 17:15:16 -0400

    Trying event loader again

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ fs.readdir('./functions/', (err, files) => {
 });
 
 
-/*fs.readdir('./events/', (err, files) => {
+fs.readdir('./events/', (err, files) => {
     if (err) console.error(err);
     console.cyan(`Attempting to load a total of ${files.length} events into the memory.`, false);
     files.forEach(file => {
@@ -77,12 +77,12 @@ fs.readdir('./functions/', (err, files) => {
             delete require.cache[require.resolve(`./events/${file}`)];
         }
         catch(err){
-            console.red(`An error has occured trying to load a event. Here is the error.`)
-            console.red(err)
+            console.red('An error has occured trying to load a event. Here is the error.');
+            console.red(err);
         }
     });
-    console.green(`Events Loading complete!`)
-});*/
+    console.green('Events Loading complete!');
+});
 
 
 
@@ -160,43 +160,6 @@ bot.on('ready', () => {
         }
         rl.prompt();
     });
-});
-bot.on('messageCreate', (msg) => {
-
-    let prefix = '';
-    if (msg.author.bot) return;
-    if (msg.channel.guild){
-        if (bot.settings.get(msg.channel.guild.id) == undefined){
-            bot.settings.set(msg.channel.guild.id, {'prefix': 'b!', 'modlogs':'mod-logs', 'welcome': 0, 'welcomeMessage': 'Welcome to the server {mention}', 'welcomeChannel' : 'general', 'Perm2': 'Trusted', 'Perm3': 'Moderator', 'Perm4' : 'Admin', 'Perm5': 'Owner'});
-        }
-        prefix = bot.settings.get(msg.channel.guild.id).prefix;
-    }
-    else{
-        prefix = 'b!';
-    }
-    if (!msg.content.startsWith(prefix))return;
-    let args = msg.content.slice(prefix.length).trim().split(/ +/g);
-    let command = args.shift();
-    let cmd;
-    if (bot.commands.has(command)) {
-        cmd = bot.commands.get(command);
-    } else if (bot.aliases.has(command)) {
-        cmd = bot.commands.get(bot.aliases.get(command));
-    }
-    if (cmd) {
-        if(cmd.conf.guildOnly == true){
-            if(!msg.channel.guild){
-                return msg.channel.createMessage('This command can only be ran in a guild.');
-            }
-        }
-        try{
-            msg.channel.sendTyping();
-            cmd.run(bot, msg, args, prefix);
-        }
-        catch(e){
-            msg.channel.createMessage({ embed: bot.errorMessage(bot, e.stack) });
-        }
-    }
 });
 
 //Events
---
+++ events/messageCreate.js
@@ -0,0 +1,36 @@
+module.exports = (bot, msg) => {
+    let prefix = '';
+    if (msg.author.bot) return;
+    if (msg.channel.guild){
+        if (bot.settings.get(msg.channel.guild.id) == undefined){
+            bot.settings.set(msg.channel.guild.id, {'prefix': 'b!', 'modlogs':'mod-logs', 'welcome': 0, 'welcomeMessage': 'Welcome to the server {mention}', 'welcomeChannel' : 'general', 'Perm2': 'Trusted', 'Perm3': 'Moderator', 'Perm4' : 'Admin', 'Perm5': 'Owner'});
+        }
+        prefix = bot.settings.get(msg.channel.guild.id).prefix;
+    }
+    else{
+        prefix = 'b!';
+    }
+    if (!msg.content.startsWith(prefix))return;
+    let args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    let command = args.shift();
+    let cmd;
+    if (bot.commands.has(command)) {
+        cmd = bot.commands.get(command);
+    } else if (bot.aliases.has(command)) {
+        cmd = bot.commands.get(bot.aliases.get(command));
+    }
+    if (cmd) {
+        if(cmd.conf.guildOnly == true){
+            if(!msg.channel.guild){
+                return msg.channel.createMessage('This command can only be ran in a guild.');
+            }
+        }
+        try{
+            msg.channel.sendTyping();
+            cmd.run(bot, msg, args, prefix);
+        }
+        catch(e){
+            msg.channel.createMessage({ embed: bot.errorMessage(bot, e.stack) });
+        }
+    }
+}
