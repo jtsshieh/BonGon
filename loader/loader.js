1a6a1afed6c3f71dbbb397058ba09cf56933ba9a
jtsshieh <jtsshieh@outlook.com>
2017-10-23 23:07:55 -0400

    Loader Typos

--- loader/loader.js
+++ loader/loader.js
@@ -3,11 +3,9 @@ const funLoader = require('./functionLoader.js');
 const eveLoader = require('./eventLoader.js');
 const strLoader = require('./structureLoader.js');
 
-module.exports = (bot) => {
-    async function load(){
-        await cmdLoader.load(bot);
-        await funLoader.load(bot);
-        await eveLoader.load(bot);
-        await strLoader.load(bot);
-    }
+module.exports = async(bot) => {
+    await cmdLoader.load(bot);
+    await funLoader.load(bot);
+    await eveLoader.load(bot);
+    await strLoader.load(bot);
 }
