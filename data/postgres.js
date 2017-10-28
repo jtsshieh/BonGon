1ebe7e52ca58174c95d7d151302c5ca468e691f3
jtsshieh <jtsshieh@outlook.com>
2017-10-27 22:40:54 -0400

    More postgres things

--- data/postgres.js
+++ data/postgres.js
@@ -1,12 +1,7 @@
-const { Client } = require('pg');
+const pg = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+const client = new pg.Client(process.env.DATABASE_URL);
 
 client.connect();
 
-const query = client.query('CREATE TABLE settings (id INT NOT NULL PRIMARY KEY, guild_id VARCHAR (20) NOT NULL, joinMessage VARCHAR (100) NOT NULL, leaveMessage VARCHAR (100) NOT NULL, banMessage VARCHAR (100) NOT NULL, unbanMessage VARCHAR (100) NOT NULL, kickMessage VARCHAR (100) NOT NULL, joinEnabled BOOL NOT NULL, leaveEnabled BOOL NOT NULL, banEnabled BOOL NOT NULL, unbanEnabled BOOL NOT NULL, kickEnabled BOOL NOT NULL, prefix VARCHAR (5) NOT NULL, memberLogChannel VARCHAR (50) NOT NULL, memberLogEnabled BOOL NOT NULL, modlogChannel VARCHAR (50) NOT NULL, modLogEnabled BOOL NOT NULL, Perm1Role VARCHAR (50) NOT NULL, Perm2Role VARCHAR (50) NOT NULL, Perm3Role VARCHAR (50) NOT NULL, Perm4Role VARCHAR (50) NOT NULL, Perm5Role VARCHAR (50) NOT NULL);');
-
-query.on('end', () => { client.end(); });
+client.query('CREATE TABLE IF NOT EXISTS settings (id INT NOT NULL PRIMARY KEY, guild_id VARCHAR (20) NOT NULL, joinMessage VARCHAR (100) NOT NULL, leaveMessage VARCHAR (100) NOT NULL, banMessage VARCHAR (100) NOT NULL, unbanMessage VARCHAR (100) NOT NULL, kickMessage VARCHAR (100) NOT NULL, joinEnabled BOOL NOT NULL, leaveEnabled BOOL NOT NULL, banEnabled BOOL NOT NULL, unbanEnabled BOOL NOT NULL, kickEnabled BOOL NOT NULL, prefix VARCHAR (5) NOT NULL, memberLogChannel VARCHAR (50) NOT NULL, memberLogEnabled BOOL NOT NULL, modlogChannel VARCHAR (50) NOT NULL, modLogEnabled BOOL NOT NULL, Perm1Role VARCHAR (50) NOT NULL, Perm2Role VARCHAR (50) NOT NULL, Perm3Role VARCHAR (50) NOT NULL, Perm4Role VARCHAR (50) NOT NULL, Perm5Role VARCHAR (50) NOT NULL);');
