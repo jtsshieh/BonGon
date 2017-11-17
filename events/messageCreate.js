84c3c35131834e6ffba060737fc333365b180906
jtsshieh <jtsshieh@outlook.com>
2017-11-16 20:50:59 -0500

    restore messagecreate

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,43 +1,36 @@
 module.exports = async (bot, msg) => {
-  if (msg.channel.permissionsOf(bot.user.id).has('sendMessages')) {
-    let prefix = '';
-    if (msg.author.bot) return;
-
-    if (msg.channel.guild) {
-      prefix = bot.settings.get(msg.channel.guild.id).prefix;
+  let prefix = '';
+  if (msg.author.bot) return;
+  if (msg.channel.guild) {
+    if (bot.settings.get(msg.channel.guild.id) == undefined) {
+      bot.settings.set(msg.channel.guild.id, {'prefix': 'b!', 'modlogs':'mod-logs', 'welcome': 0, 'welcomeMessage': 'Welcome to the server {mention}', 'welcomeChannel' : 'mainchat', 'Perm2': 'Trusted', 'Perm3': 'Moderator', 'Perm4' : 'Admin', 'Perm5': 'Owner'});
     }
-    else {
-      prefix = 'b!';
-    }
-
-    if (!msg.content.startsWith(prefix)) return;
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift();
-
-    let cmd;
-
-    if (bot.commands.has(command)) {
-      cmd = bot.commands.get(command);
-    } else if (bot.aliases.has(command)) {
-      cmd = bot.commands.get(bot.aliases.get(command));
-    }
-
-    if (cmd) {
-      if (cmd.conf.guildOnly == true) {
-        if (!msg.channel.guild) {
-          return await msg.channel.createMessage('This command can only be ran in a guild.');
-        }
-      }
-      try {
-        await msg.channel.sendTyping();
-        cmd.run(bot, msg, args, prefix);
-      }
-      catch (e) {
-        await msg.channel.createMessage({ embed: bot.errorMessage(bot, e.stack) });
-      }
-    }
+    prefix = bot.settings.get(msg.channel.guild.id).prefix;
   }
   else {
-    return;
+    prefix = 'b!';
+  }
+  if (!msg.content.startsWith(prefix)) return;
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift();
+  let cmd;
+  if (bot.commands.has(command)) {
+    cmd = bot.commands.get(command);
+  } else if (bot.aliases.has(command)) {
+    cmd = bot.commands.get(bot.aliases.get(command));
+  }
+  if (cmd) {
+    if (cmd.conf.guildOnly == true) {
+      if (!msg.channel.guild) {
+        return await msg.channel.createMessage('This command can only be ran in a guild.');
+      }
+    }
+    try {
+      await msg.channel.sendTyping();
+      cmd.run(bot, msg, args, prefix);
+    }
+    catch (e) {
+      await msg.channel.createMessage({ embed: bot.errorMessage(bot, e.stack) });
+    }
   }
 };
